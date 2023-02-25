bf2741ae89ef9370ca32c514783affba624b9faf
shelby <shelbynarnold@yahoo.com>
2023-02-24 19:09:02 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// Write your code here
+
+document.getElementById("main").remove()
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+const text = document.createTextNode("Sneb is the champion")
+newHeader.appendChild(text)
